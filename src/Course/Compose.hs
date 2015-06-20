8315467f7333959d382a4073c0e2aeeb8e96e197
John Redmond <redmondjohnf@gmail.com>
2015-06-20 22:33:25 +0930

    finished Copose

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -17,23 +17,20 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  f <$> Compose fogA = Compose ((f <$>) <$> fogA)
 
 instance (Apply f, Apply g) =>
   Apply (Compose f g) where
 -- Implement the (<*>) function for an Apply instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  Compose f <*> Compose a = Compose (lift2 (<*>) f a)
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
-  pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+  pure a = Compose (pure . pure $ a)
 
 instance (Bind f, Bind g) =>
   Bind (Compose f g) where
 -- Implement the (=<<) function for a Bind instance for Compose
   (=<<) =
-    error "todo: Course.Compose (<<=)#instance (Compose f g)"
+    error "impossible"
